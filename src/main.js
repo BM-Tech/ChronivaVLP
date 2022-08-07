559bf7fea3fab5ef660685561c84bf0bc0826c14
benman604 <benjamin.man1001@gmail.com>
2022-08-06 23:56:00 -0700

    default to alg 1

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import App from './App.svelte';
 
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
-const playlistId = urlParams.get('id')
+let playlistId = urlParams.get('id')
+if (playlistId == null) {
+	playlistId = "PL9i49hRn8Tk8qD2QxNyR_0TvllCrYNG23"
+}
 
 const app = new App({
 	target: document.body,
